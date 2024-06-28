e98f68ecea63df8f70f904f3e23051a908c2eeb1
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-06-28 16:27:12 +0100

    update favicons and added opengraph-about-image

---
+++ src/routes/about/+layout.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<meta name="description" content="About me" />
+	<meta name="keywords" content="About me" />
+	<meta property="og:title" content="Ibrahim Raimi - About me" />
+	<meta property="og:description" content="About me - my interests, random things i like" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://ibrahimraimi.com" />
+	<meta property="og:image" content="https://ibrahimraimi.com/opengraph-about-image.png" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Ibrahim Raimi - About me" />
+	<meta name="twitter:description" content="About me - my interests, random things i like" />
+	<meta name="twitter:image" content="https://ibrahimraimi.com/opengraph-about-image.png" />
+	<title>about me</title>
+</svelte:head>
+
+<slot />
