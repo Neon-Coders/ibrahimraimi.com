0ead60428e7e5b98b3529f603705519cc0ae9412
Metro <66981941+ibrahimraimi@users.noreply.github.com>
2024-10-04 16:50:09 +0100

    Update site-header.svelte

--- src/lib/components/site-header.svelte
+++ src/lib/components/site-header.svelte
@@ -2,11 +2,6 @@
 	<nav class="flex h-16 items-center justify-between text-center">
 		<a href="/" class=""><span>Ibrahim</span></a>
 
-		<ul class="flex items-center justify-center text-[#9ca3af]">
-			<li class="px-2"><a href="/work">work</a></li>
-			<li class="px-2"><a href="/about">about</a></li>
-			<li class="px-2"><a href="/craft">craft</a></li>
-			<!-- <li class="px-2"><a href="https://dev.to/ibrahimraimi" target="_blank">blog</a></li> -->
-		</ul>
+		<ul class="flex items-center justify-center text-[#9ca3af]"></ul>
 	</nav>
 </header>
